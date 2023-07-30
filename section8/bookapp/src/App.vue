d6012f8211b9f3209ca9d1a0dd0f6996a4c3b9eb
Naru-hub <1208ny0324@gmail.com>
2023-07-30 22:01:27 +0900

    localStorageへ保存, searchからeditへの移動と表示, beforeRouteEnter+, pickerなど

--- section8/bookapp/src/App.vue
+++ section8/bookapp/src/App.vue
@@ -2,7 +2,9 @@
   <v-app>
     <Header />
     <v-main>
-      <router-view />
+      <v-container>
+        <router-view :books="books" @add-book-list="addBook" />
+      </v-container>
     </v-main>
     <Footer />
   </v-app>
@@ -11,6 +13,7 @@
 <script>
 import Header from "@/global/Header";
 import Footer from "@/global/Footer";
+const STORAGE_KEY = "books";
 
 export default {
   name: "App",
@@ -19,8 +22,47 @@ export default {
     Footer,
   },
 
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+      books: [],
+      newBook: null,
+    };
+  },
+  mounted() {
+    if (localStorage.getItem(STORAGE_KEY)) {
+      try {
+        this.books = JSON.parse(localStorage.getItem(STORAGE_KEY));
+      } catch (e) {
+        localStorage.removeItem(STORAGE_KEY);
+      }
+    }
+  },
+  methods: {
+    addBook(e) {
+      this.books.push({
+        id: this.books.length,
+        title: e.title,
+        image: e.image,
+        description: e.description,
+        readDate: "",
+        memo: "",
+      });
+      // this.newBook = "";
+      this.saveBooks();
+      // 最後に追加したidの取得コード
+      this.goToEditPage(this.books.slice(-1)[0].id);
+    },
+    removeBook(x) {
+      this.books.splice(x, 1);
+      this.saveBooks();
+    },
+    saveBooks() {
+      const parsed = JSON.stringify(this.books);
+      localStorage.setItem(STORAGE_KEY, parsed);
+    },
+    goToEditPage(id) {
+      this.$router.push(`/edit/${id}`);
+    },
+  },
 };
 </script>
